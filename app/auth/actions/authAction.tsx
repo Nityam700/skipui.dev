da5ddd71508181c1bc2f9f92ad0ab5128f034d8e
Nityam Kumar <nityam950@gmail.com>
2024-02-23 14:22:03 +0530

    Alpha Stage

---
+++ app/auth/actions/authAction.tsx
@@ -0,0 +1,9 @@
+"use server"
+
+export async function authSignIn(){
+    // sign in logic
+}
+
+export async function authSignOut(){
+    // sign out logic
+}
---
+++ app/auth/actions/getUser.tsx
@@ -0,0 +1,14 @@
+"use server"
+
+export async function getUser(){
+    // get user using current session
+}
+export async function getUserId(){
+    // get user by unique id
+}
+export async function getUserRole(){
+    // get user's role
+}
+export async function getUserUserName(){
+    // get user's username
+}
